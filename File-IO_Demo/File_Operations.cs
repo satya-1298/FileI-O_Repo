83bd02159f76e1f4772ce0fcb53a49635ef207a0
unknown <satya-1298>
2023-07-24 19:09:20 +0530

    [Satya]Add : Checking that given file path exist are not

---
+++ File-IO_Demo/File_Operations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO_Demo
+{
+    public class File_Operations
+    {
+        public string path = @"C:\Users\PC\Desktop\RFP288\FileI-O_Repo\File-IO_Demo\File\Satya.txt";
+        public void Check()
+        {
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+            else
+            {
+                Console.WriteLine("File not exists");
+            }
+        }
+       
+    }
+}
---
+++ File-IO_Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            File_Operations file_Operations = new File_Operations();
+            while (true)
+            {
+                Console.WriteLine("1");
+                Console.WriteLine("Choose the option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        file_Operations.Check();
+                        break;
+                    
+                }
+            }
+        }
+    }
+}
